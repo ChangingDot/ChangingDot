17608c1ecf640c8a85e7ed4cf9098ef480e2e347
Matthieu Rossignon <matthieu.rossignon@hotmail.fr>
2024-07-26 11:36:47 +0200

    feat: we can now give a local or remote path for config

--- back/src/cli/cli.py
+++ back/src/cli/cli.py
@@ -1,6 +1,9 @@
 import json
+from typing import Any
+from urllib.parse import urlparse
 
 import click
+import requests
 import yaml
 from core.commit_graph import run_commit_graph_from_config
 from core.create_graph import run_create_graph
@@ -15,6 +18,34 @@ from core.resume_graph import run_resume_graph
 from visualize.main import visualize_graph
 
 from cli.setup_feedback_server import feedback_server
+
+
+def is_url(path: str) -> bool:
+    try:
+        result = urlparse(path)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
+def get_config_dict(path: str) -> Any:
+    if is_url(path):
+        response = requests.get(path)
+        if response.status_code == 200:
+            config_content = response.text
+            return yaml.safe_load(config_content)
+        else:
+            raise click.ClickException(
+                f"Failed to fetch the config file from URL. Status code: {response.status_code}"
+            )
+    else:
+        try:
+            with open(path) as file:
+                return yaml.safe_load(file)
+        except Exception as e:
+            raise click.ClickException(
+                f"Failed to read the config file from local path. Error: {e}"
+            ) from e
 
 
 @click.group()
@@ -32,8 +63,7 @@ def cdot() -> None:
 @click.option("--local/--remote", default=True)
 @click.option("--dev", is_flag=True, default=False)
 def create(config: str, local: bool, dev: bool) -> None:
-    with open(config) as file:
-        config_dict = yaml.safe_load(file)
+    config_dict = get_config_dict(config)
     create_graph_input = CreateGraphInput(**config_dict, is_local=local)
     if dev:
         run_create_graph(
@@ -68,8 +98,7 @@ def create(config: str, local: bool, dev: bool) -> None:
 )
 @click.option("--local/--remote", default=True)
 def visualize(config: str, local: bool) -> None:
-    with open(config) as file:
-        config_dict = yaml.safe_load(file)
+    config_dict = get_config_dict(config)
     visualize_graph(config_dict, local)
 
 
@@ -82,8 +111,7 @@ def visualize(config: str, local: bool) -> None:
 )
 @click.option("--local/--remote", default=True)
 def optimize(config: str, local: bool) -> None:
-    with open(config) as file:
-        config_dict = yaml.safe_load(file)
+    config_dict = get_config_dict(config)
     optimize_graph_input = OptimizeGraphInput(**config_dict, is_local=local)
     run_optimize_graph(
         optimize_graph_input.iteration_name,
@@ -109,8 +137,7 @@ def optimize(config: str, local: bool) -> None:
 )
 @click.option("--dev", is_flag=True, default=False)
 def resume(config: str, local: bool, resume_node: str, dev: bool) -> None:
-    with open(config) as file:
-        config_dict = yaml.safe_load(file)
+    config_dict = get_config_dict(config)
     resume_initial_node = ResumeInitialNode(**json.loads(resume_node))
     resume_graph_input = ResumeGraphInput(
         **config_dict, is_local=local, resume_initial_node=resume_initial_node
@@ -152,8 +179,7 @@ def resume(config: str, local: bool, resume_node: str, dev: bool) -> None:
 )
 @click.option("--local/--remote", default=True)
 def commit(config: str, local: bool) -> None:
-    with open(config) as file:
-        config_dict = yaml.safe_load(file)
+    config_dict = get_config_dict(config)
     run_commit_graph_from_config(config_dict, local)
 
 
