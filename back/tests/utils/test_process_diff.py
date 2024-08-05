570e3ed1f823650e5fc83b49615678b9c62ce437
Matthieu Rossignon <matthieu.rossignon@hotmail.fr>
2024-08-05 10:22:58 +0200

    test: add some edge cases for process_diff

--- back/tests/utils/test_process_diff.py
+++ back/tests/utils/test_process_diff.py
@@ -72,3 +72,153 @@ random blabla
     }""",
         )
     ]
+
+
+def test_processing_multiple_diffs_in_single_block() -> None:
+    diff = """
+    random blabla
+```diff
+--- file.cs
++++ file.cs
+@@ ... @@
+-        var x = 5;
++        var x = 10;
+something
+-        Console.WriteLine("Hello");
++        Console.WriteLine("World");
+random blabla
+"""
+
+    assert process_diff(diff) == [
+        ProcessedDiff(
+            before="        var x = 5;",
+            after="        var x = 10;",
+        ),
+        ProcessedDiff(
+            before='        Console.WriteLine("Hello");',
+            after='        Console.WriteLine("World");',
+        ),
+    ]
+
+
+def test_processing_multiple_diffs_in_single_block_no_divider() -> None:
+    diff = """
+    random blabla
+```diff
+--- file.cs
++++ file.cs
+@@ ... @@
+-        var x = 5;
++        var x = 10;
+-        Console.WriteLine("Hello");
++        Console.WriteLine("World");
+random blabla
+"""
+
+    assert process_diff(diff) == [
+        ProcessedDiff(
+            before="        var x = 5;",
+            after="        var x = 10;",
+        ),
+        ProcessedDiff(
+            before='        Console.WriteLine("Hello");',
+            after='        Console.WriteLine("World");',
+        ),
+    ]
+
+
+def test_processing_diff_with_added_lines_only() -> None:
+    diff = """
+    random blabla
+    diffCopy--- file.cs
+    +++ file.cs
+    @@ ... @@
+public class Example
+{
++    private string _name;
+    public void Method()
+    {
+        // Some comment
+    }
+}
+    random blabla
+    """
+    assert process_diff(diff) == [
+        ProcessedDiff(
+            before="",
+            after="    private string _name;",
+        )
+    ]
+
+
+def test_processing_diff_with_remove_lines_only() -> None:
+    diff = """
+    random blabla
+    diffCopy--- file.cs
+    +++ file.cs
+    @@ ... @@
+public class Example
+{
+-    private string _name;
+    public void Method()
+    {
+        // Some comment
+    }
+}
+    random blabla
+    """
+    assert process_diff(diff) == [
+        ProcessedDiff(
+            before="    private string _name;",
+            after="",
+        )
+    ]
+
+
+def test_processing_diff_with_line_jumps() -> None:
+    diff = """
+    random blabla
+    diffCopy--- file.cs
+    +++ file.cs
+    @@ ... @@
+public class Example
+{
++    private string _name;
++
++
+    public void Method()
+    {
+        // Some comment
+    }
+}
+    random blabla
+    """
+    assert process_diff(diff) == [
+        ProcessedDiff(
+            before="",
+            after="""    private string _name;
+
+""",
+        )
+    ]
+
+
+def test_processing_diff_when_operators_are_not_first_char() -> None:
+    diff = """
+    random blabla
+    diffCopy--- file.cs
+    +++ file.cs
+    @@ ... @@
+public class Example
+{
+    public void Method()
+    {
+    -var a = "a";
+    +var b = "b";
+    }
+}
+    random blabla
+    """
+    assert process_diff(diff) == [
+        ProcessedDiff(before='    var a = "a";', after='    var b = "b";')
+    ]
