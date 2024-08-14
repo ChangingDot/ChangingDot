d9ec4b5f443a27d45facd078df6b8d706e8ac6c8
Matthieu Rossignon <matthieu.rossignon@hotmail.fr>
2024-08-14 15:07:27 +0200

    refactor: remove diffing generation and generate all block code instead

--- back/src/changing_dot/instruction_interpreter/block_prompts.py
+++ back/src/changing_dot/instruction_interpreter/block_prompts.py
@@ -5,55 +5,15 @@ You always COMPLETELY IMPLEMENT the needed code!
 Always use best practices when coding.
 Respect and use existing conventions, libraries, etc that are already present in the code base.
 
-For each file that needs to be changed, write out the changes similar to a unified diff like `diff -U0` would produce. For example:
-
-# Example conversation 1
-1. Add an imports of sympy.
-2. Remove the is_prime() function.
-3. Replace the existing call to is_prime() with a call to sympy.isprime().
-
-Here are the diffs for those changes:
-
-```diff
---- mathweb/flask/app.py
-+++ mathweb/flask/app.py
-@@ ... @@
--class MathWeb:
-+import sympy
-+
-+class MathWeb:
-@@ ... @@
--def is_prime(x):
--    if x < 2:
--        return False
--    for i in range(2, int(math.sqrt(x)) + 1):
--        if x % i == 0:
--            return False
--    return True
-@@ ... @@
--@app.route('/prime/<int:n>')
--def nth_prime(n):
--    count = 0
--    num = 1
--    while count < n:
--        num += 1
--        if is_prime(num):
--            count += 1
--    return str(num)
-+@app.route('/prime/<int:n>')
-+def nth_prime(n):
-+    count = 0
-+    num = 1
-+    while count < n:
-+        num += 1
-+        if sympy.isprime(num):
-+            count += 1
-+    return str(num)
-```
+For each code block that you are given, rewrite the code block with the expected changes. Do not add other code that should not be in the code block ( for example imports when modifying a method code block ), and write the complete code block, since it will be added as is to the code base
 """
 
 edits_template = """
 Please follow the following instructions {solution}
-Here are the only lines that need to be modified, only modify lines from this block, changes outside of this block will not be taken into account, or will crash the algorithm :
-"{content}" :
+Here is the block that need to be modified :
+```
+{content}
+```
+
+Please write the modified code block :
 """
--- back/tests/core/test_block_interpreter.py
+++ back/tests/core/test_block_interpreter.py
@@ -1,6 +1,5 @@
 from typing import TYPE_CHECKING
 
-import pytest
 from changing_dot.custom_types import BlockEdit, EmptyEdit
 from changing_dot.dependency_graph.dependency_graph import DependencyGraph
 from changing_dot.instruction_interpreter.block_instruction_interpreter import (
@@ -52,19 +51,12 @@ def test_basic_change() -> None:
 
     interpreter = make_instruction_interpreter(
         """
-    random blabla
-```diff
---- file.cs
-+++ file.cs
-@@ ... @@
--    static string SimpleMethod()
--    {
--        return "Hello, World!";
--    }
-+    static string SimpleMethod()
-+    {
-+        return "Welcome, World!";
-+    }
+Here is the new block
+```csharp
+    static string SimpleMethod()
+    {
+        return "Welcome, World!";
+    }
 ```
 
 random blabla
@@ -85,115 +77,3 @@ random blabla
     )
 
     assert expected_edit == interpreter.get_edit_from_instruction(instruction, DG)
-
-
-def test_basic_change_with_plus_before_minus() -> None:
-    DG = DependencyGraph(["./tests/core/fixtures/block_interpreter/subject.cs"])
-    instruction: InstructionBlock = {
-        "file_path": "./tests/core/fixtures/block_interpreter/subject.cs",
-        "block_id": 1,
-        "solution": "Change Hello, World! to Welcome, World!",
-    }
-
-    interpreter = make_instruction_interpreter(
-        """
-    random blabla
-```diff
---- file.cs
-+++ file.cs
-@@ ... @@
-+    static string SimpleMethod()
-+    {
-+        return "Welcome, World!";
-+    }
--    static string SimpleMethod()
--    {
--        return "Hello, World!";
--    }
-```
-
-random blabla
-    """
-    )
-
-    expected_edit: BlockEdit = BlockEdit(
-        file_path="./tests/core/fixtures/block_interpreter/subject.cs",
-        block_id=1,
-        before="""static string SimpleMethod()
-    {
-        return "Hello, World!";
-    }""",
-        after="""static string SimpleMethod()
-    {
-        return "Welcome, World!";
-    }""",
-    )
-
-    assert expected_edit == interpreter.get_edit_from_instruction(instruction, DG)
-
-
-def test_basic_change_but_LLM_returns_changes_not_full_block() -> None:
-    DG = DependencyGraph(["./tests/core/fixtures/block_interpreter/subject.cs"])
-    instruction: InstructionBlock = {
-        "file_path": "./tests/core/fixtures/block_interpreter/subject.cs",
-        "block_id": 1,
-        "solution": "Change Hello, World! to Welcome, World!",
-    }
-
-    interpreter = make_instruction_interpreter(
-        """
-    random blabla
-```diff
---- file.cs
-+++ file.cs
-@@ ... @@
--        return "Hello, World!";
-+        return "Welcome, World!";
-```
-
-random blabla
-    """
-    )
-
-    expected_edit: BlockEdit = BlockEdit(
-        file_path="./tests/core/fixtures/block_interpreter/subject.cs",
-        block_id=1,
-        before="""static string SimpleMethod()
-    {
-        return "Hello, World!";
-    }""",
-        after="""static string SimpleMethod()
-    {
-        return "Welcome, World!";
-    }""",
-    )
-
-    assert expected_edit == interpreter.get_edit_from_instruction(instruction, DG)
-
-
-def test_that_no_context_add_fails() -> None:
-    DG = DependencyGraph(["./tests/core/fixtures/block_interpreter/subject.cs"])
-    instruction: InstructionBlock = {
-        "file_path": "./tests/core/fixtures/block_interpreter/subject.cs",
-        "block_id": 1,
-        "solution": "Change Hello, World! to Welcome, World!",
-    }
-
-    interpreter = make_instruction_interpreter(
-        """
-  There is no line number between @@
-```diff
---- file.cs
-+++ file.cs
-@@ ... @@
-+        add a new line
-```
-
-random blabla
-    """
-    )
-
-    with pytest.raises(
-        ValueError, match="Code was added, but we do not know where to put it"
-    ):
-        interpreter.get_edit_from_instruction(instruction, DG)
