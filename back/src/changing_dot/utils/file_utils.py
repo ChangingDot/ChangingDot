47d12d2ed64b2fe65c221b542471ab8e57c2e835
Matthieu Rossignon <matthieu.rossignon@hotmail.fr>
2024-08-27 15:25:10 +0200

    feat: new dependency graph constructor using folder path and supported language

--- back/src/changing_dot/utils/file_utils.py
+++ back/src/changing_dot/utils/file_utils.py
@@ -7,6 +7,27 @@ def get_file_extension(file_name: str) -> str:
     return file_extension[1:]
 
 
+def get_python_files(dir_to_explore: str) -> list[str]:
+    if not os.path.exists(dir_to_explore):
+        raise FileNotFoundError(f"The path {dir_to_explore} does not exist.")
+
+    python_files = glob.glob(os.path.join(dir_to_explore, "**", "*.py"), recursive=True)
+
+    exclude_patterns = [
+        os.path.sep + "__pycache__" + os.path.sep,
+        ".pyc",
+        ".pyo",
+        ".pyd",
+    ]
+
+    filtered_files = []
+    for file in python_files:
+        if not any(pattern in file for pattern in exclude_patterns):
+            filtered_files.append(os.path.abspath(file))
+
+    return filtered_files
+
+
 def get_csharp_files(dir_to_explore: str) -> list[str]:
     if not os.path.exists(dir_to_explore):
         raise FileNotFoundError(f"The path {dir_to_explore} does not exist.")
