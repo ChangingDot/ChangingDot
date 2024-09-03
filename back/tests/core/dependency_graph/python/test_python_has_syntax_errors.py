bf6ad76ea5608b48d65b3e95b0a4a6c11423e733
Matthieu Rossignon <matthieu.rossignon@hotmail.fr>
2024-09-03 09:57:07 +0200

    test: split test_has_syntax_errors tests into language specific files

--- back/tests/core/dependency_graph/python/test_python_has_syntax_errors.py
+++ back/tests/core/dependency_graph/python/test_python_has_syntax_errors.py
@@ -5,6 +5,6 @@ def get_fixture_path(file_path: str) -> str:
     return "./tests/core/dependency_graph/fixtures/syntax_errors/" + file_path
 
 
-def test_empty_list() -> None:
-    graph = DependencyGraph([])
-    assert not graph.has_syntax_errors()
+def test_syntax_error_python() -> None:
+    graph = DependencyGraph([get_fixture_path("has_syntax_error.py")])
+    assert graph.has_syntax_errors()
