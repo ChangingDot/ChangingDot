dcda1cf76580562e71dd08b76733e6e898bf0de5
Matthieu Rossignon <matthieu.rossignon@hotmail.fr>
2024-09-03 11:58:01 +0200

    refactor: simplify language matcher

--- back/src/changing_dot/dependency_graph/language_matchers.py
+++ back/src/changing_dot/dependency_graph/language_matchers.py
@@ -1,87 +1,79 @@
 from changing_dot.dependency_graph.types import (
     DependencyGraphNodeType,
-    NodeTypeToTerminal,
 )
 from tree_sitter import Node
 
+TerminalToNodeType = dict[str, DependencyGraphNodeType]
+
 
 class ILanguageMatcher:
-    def match_class(self, node_type: DependencyGraphNodeType, ast_node: Node) -> bool:
+    def match_class(self, ast_node: Node) -> DependencyGraphNodeType | None:
         raise NotImplementedError()
 
 
 class EmptyMatcher(ILanguageMatcher):
-    def match_class(self, node_type: DependencyGraphNodeType, ast_node: Node) -> bool:
-        return False
+    def match_class(self, ast_node: Node) -> DependencyGraphNodeType | None:
+        return None
 
 
-c_sharp_node_type_to_terminal: NodeTypeToTerminal = {
-    "Class": ["class_declaration"],
-    "Method": ["method_declaration"],
-    "Constructor": ["constructor_declaration"],
-    "Field": ["field_declaration", "property_declaration"],
-    "Import": ["using_directive"],
+c_sharp_terminal_to_node_type: TerminalToNodeType = {
+    "class_declaration": "Class",
+    "method_declaration": "Method",
+    "constructor_declaration": "Constructor",
+    "field_declaration": "Field",
+    "property_declaration": "Field",
+    "using_directive": "Import",
 }
 
 
 class CSharpMatcher(ILanguageMatcher):
-    def match_class(self, node_type: DependencyGraphNodeType, ast_node: Node) -> bool:
-        return ast_node.type in c_sharp_node_type_to_terminal[node_type]
+    def match_class(self, ast_node: Node) -> DependencyGraphNodeType | None:
+        return c_sharp_terminal_to_node_type.get(ast_node.type)
 
 
-python_node_type_to_terminal: NodeTypeToTerminal = {
-    "Class": ["class_definition"],
-    "Method": ["function_definition"],
-    "Constructor": [],
-    "Field": [],
-    "Import": ["import_statement"],
+python_terminal_to_node_type: TerminalToNodeType = {
+    "class_definition": "Class",
+    "function_definition": "Method",
+    "import_statement": "Import",
 }
 
 
 class PythonMatcher(ILanguageMatcher):
-    def match_class(self, node_type: DependencyGraphNodeType, ast_node: Node) -> bool:
-        if node_type != "Constructor" and node_type != "Method":
-            return ast_node.type in python_node_type_to_terminal[node_type]
-        else:
-            if node_type == "Constructor":
-                return (
-                    ast_node.type in python_node_type_to_terminal["Method"]
-                    and ast_node.text is not None
-                    and "def __init__(" in ast_node.text.decode("utf-8")
-                )
-            elif node_type == "Method":
-                return ast_node.type in python_node_type_to_terminal["Method"] and not (
-                    ast_node.text is not None
-                    and "def __init__(" in ast_node.text.decode("utf-8")
-                )
+    def match_class(self, ast_node: Node) -> DependencyGraphNodeType | None:
+        node_type = python_terminal_to_node_type.get(ast_node.type)
+
+        if (
+            node_type == "Method"
+            and ast_node.text is not None
+            and "def __init__(" in ast_node.text.decode("utf-8")
+        ):
+            node_type = "Constructor"
+
+        return node_type
 
 
-pip_req_node_type_to_terminal: NodeTypeToTerminal = {
-    "Class": [],
-    "Method": ["requirement"],
-    "Constructor": [],
-    "Field": [],
-    "Import": [],
+pip_req_terminal_to_node_type: TerminalToNodeType = {
+    "requirement": "Method",
 }
 
 
 class PipReqMatcher(ILanguageMatcher):
-    def match_class(self, node_type: DependencyGraphNodeType, ast_node: Node) -> bool:
-        return ast_node.type in pip_req_node_type_to_terminal[node_type]
+    def match_class(self, ast_node: Node) -> DependencyGraphNodeType | None:
+        return pip_req_terminal_to_node_type.get(ast_node.type)
 
 
 class XmlMatcher(ILanguageMatcher):
-    def match_class(self, node_type: DependencyGraphNodeType, ast_node: Node) -> bool:
-        if node_type != "Method":
-            return False
-        return ast_node.type == "element"
+    def match_class(self, ast_node: Node) -> DependencyGraphNodeType | None:
+        if ast_node.type == "element":
+            return "Method"
+        return None
 
 
 class CsProjMatcher(ILanguageMatcher):
-    def match_class(self, node_type: DependencyGraphNodeType, ast_node: Node) -> bool:
-        if node_type != "Method":
-            return False
-        return ast_node.type == "element" and self.has_element_descendant(ast_node)
+    def match_class(self, ast_node: Node) -> DependencyGraphNodeType | None:
+        if ast_node.type == "element" and self.has_element_descendant(ast_node):
+            return "Method"
+        return None
 
     def has_element_descendant(self, node: Node) -> bool:
         for child in node.children:
